ddd66e5888e7ae6176d3680cf84d7e7c66de4e29
Eoan O Dea <odeaeoan@gmail.com>
2020-10-23 13:22:29 +0200

    Improved sort method and error recovery

--- js/api-spotify.js
+++ js/api-spotify.js
@@ -44,17 +44,32 @@ export const categories = () => {
       Authorization: "Bearer " + token,
     },
     success: function (data, status, res) {
+      /**
+       * If an error message was previously displayed
+       * empty the div
+       */
+      if ($("#error-box").length) {
+        $("#data-container").empty();
+      }
+
+      /**
+       * Get the JSON data from the response
+       */
       const response = res.responseJSON;
-      console.log("Data: ", response);
 
       $("#page-title").text("Categories");
       $("#data-container").append(
         `<div class='category-list row' id="category-list"></div>`
       );
 
+      /**
+       * Append each item to
+       * the #data-container div
+       */
       response.categories.items.forEach((item, i) => {
         $("#category-list").append(`
           <div class='category-item item-${i}'
+          key="${i}"
           style="
     background-image: url(${item.icons[0].url})"
           >
@@ -73,9 +88,13 @@ export const categories = () => {
   });
 };
 
+/**
+ * Render an error message to the screen
+ * @param {string} msg
+ */
 function renderErrorMessage(msg) {
   $("#data-container").append(
-    `<div class='error-container' id="error">
+    `<div class='error-container' id="error-box">
         <h2>Data Error</h2>
         <p>${msg}</p>
         <button id='retry-error' type="button" class="btn btn-primary">Retry</button>
@@ -86,7 +105,6 @@ function renderErrorMessage(msg) {
    * Error Recover Button Event Listener
    */
   $("#retry-error").click(function () {
-    console.log("hello");
     authorize();
   });
 }
--- js/main.js
+++ js/main.js
@@ -13,6 +13,17 @@ $(document).ready(function () {
    * Sort Data Event Listener
    */
   $("#sort-data").change(function () {
+    /**
+     * Get the selected index from the select element
+     * And all the option children
+     */
+    const selectedIndex = $(this)[0].selectedIndex;
+    const options = $(this).children();
+
+    if (options[selectedIndex].id === "default") {
+    }
+
+    console.log(options[selectedIndex].id);
     sortUsingNestedText(
       $("#data-container .row"),
       "div",
@@ -20,11 +31,16 @@ $(document).ready(function () {
     );
   });
 });
+// $("#data-container .row").children().first().attr("key");
 
 /**
  * Set up sort attributes
  */
-$("#sort-data").data("sortKey", "h2.title");
+$("#alphabetical").data("sortKey", "h2.title");
+$("#reverse").data("sortKey", "h2.title");
+$("#default").data("sortKey", function () {
+  return $(this).attr("key");
+});
 
 /**
  * Sort nested items within a jQuery array
@@ -34,11 +50,18 @@ $("#sort-data").data("sortKey", "h2.title");
  * @param {*} parent - The parent of the sorted array
  * @param {*} childSelector - A single child within that array
  * @param {*} keySelector - The item you want to sort by
+ * @param {boolean} inverse - [optional] if you want to sort is backwards
  */
-function sortUsingNestedText(parent, childSelector, keySelector) {
+function sortUsingNestedText(
+  parent,
+  childSelector,
+  keySelector,
+  inverse = false
+) {
   var items = parent.children(childSelector).sort(function (a, b) {
     var vA = $(keySelector, a).text();
     var vB = $(keySelector, b).text();
+    if (inverse) return vA > vB ? -1 : vA > vB ? 1 : 0;
     return vA < vB ? -1 : vA > vB ? 1 : 0;
   });
   parent.append(items);
