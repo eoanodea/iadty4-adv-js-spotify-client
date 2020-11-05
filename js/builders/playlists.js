bdc800b3c1d009f5f099e443c9ae3ad239a865f6
Eoan O Dea <odeaeoan@gmail.com>
2020-11-05 14:30:52 +0100

    Code cleanup and improved comments on playlists.js

--- js/builders/playlists.js
+++ js/builders/playlists.js
@@ -6,7 +6,9 @@
  */
 export const buildPlaylistArr = (item) => {
   let tracks = [];
-
+  /**
+   * Create an array of tracks
+   */
   item.tracks.items.forEach((item, i) => {
     tracks.push({
       name: item.track.name,
@@ -16,20 +18,27 @@ export const buildPlaylistArr = (item) => {
       href: item.track.external_urls.spotify,
     });
   });
+  /**
+   * Data for the playlist header
+   */
   const header = {
     name: item.name,
     description: item.description,
     followers: item.followers.total,
     owner: item.owner.display_name,
-    primary_color: item.primary_color,
   };
 
   return { tracks, header };
 };
 
 /**
- * Builds a playlist list wrapper
+ * Builds the playlist table, along with title
+ * and sub title
  *
+ * @param {String} name - The title of the playlist
+ * @param {String} description - The description of the playlist
+ * @param {int} followers - The number of followers playlist has
+ * @param {String} owner - The owner of the playlist
  */
 export const playlistList = ({ name, description, followers, owner }) => {
   /**
@@ -52,25 +61,35 @@ export const playlistList = ({ name, description, followers, owner }) => {
 `;
 };
 
+/**
+ * Displays a subheader for a playlist
+ * Showing details on it's description, follower count and owner
+ *
+ * @param {String} description
+ * @param {int} followers
+ * @param {String} owner
+ */
 export const playlistDesc = (description, followers, owner) => `
 <div class="container">
-<div class="row">
-  <p class="col">${description}</p>
-  <div class="col row justify-content-end">
-    <p class="text-muted">Followers: <br /> ${followers}</p>
-    <p class="text-muted">Created by: <br /> ${owner}</p>
-  </div>
+  <div class="row">
+    <p class="col">${description}</p>
+    <div class="col row justify-content-end">
+      <p class="text-muted">Followers: <br /> ${followers}</p>
+      <p class="text-muted">Created by: <br /> ${owner}</p>
+    </div>
   </div>
 </div>
 `;
 
 /**
+ * A single item within the playlist songs table
  *
  * @param {int} i - Index Key
- * @param {string} name - Item Title
- * @param {string} imageURL - URL to the image
+ * @param {String} name - Item Title
+ * @param {int} duration - The length of the song in miliseconds
+ * @param {String} href - The URL to the song on Spotify web
  */
-export const playlistItem = ({ i, id, name, duration, href }) => `
+export const playlistItem = ({ i, name, duration, href }) => `
 <tr class="clickable-row" data-href="${href}">
     <th scope="row">${i}</th>
     <td>${name}</td>
